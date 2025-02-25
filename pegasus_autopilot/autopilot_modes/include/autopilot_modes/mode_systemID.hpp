6d6fcda4468d3f704b354c4c39b97d8c03a70528
HenriqueSim <hen.mag.sim1@gmail.com>
2025-02-25 16:27:37 +0000

    Created mode inside pegasus autopilot

---
+++ pegasus_autopilot/autopilot_modes/include/autopilot_modes/mode_systemID.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <autopilot/mode.hpp>
+#include "pegasus_msgs/srv/system_id.hpp"
+#include "geometry_msgs/msg/vector3_stamped.hpp" // Ensure this header is included
+#include "rclcpp/clock.hpp"
+
+// // ROS2 messages
+// #include "pegasus_msgs/msg/pid_statistics.hpp"
+// #include "pegasus_msgs/msg/control_attitude.hpp"
+// #include "pegasus_msgs/msg/control_position.hpp"
+
+namespace autopilot {
+
+class SystemIdentification : public autopilot::Mode {
+
+public:
+
+    ~SystemIdentification();
+
+    void initialize() override;
+    bool enter() override;
+    bool exit() override;
+    void update(double dt) override;
+
+protected:
+
+    // The waypoint service callback
+    void systemID_callback(const pegasus_msgs::srv::SystemID::Request::SharedPtr request, const pegasus_msgs::srv::SystemID::Response::SharedPtr response);
+
+    // Check if the waypoint is already set
+    bool settings_set_{false};
+
+    Eigen::Vector3d compute_attitude(double t);
+
+    // The mass of the vehicle
+    double mass_;
+
+    // The target position and attitude waypoint to be at
+    Eigen::Vector3d axis{Eigen::Vector3d::Zero()};
+    float amplitude{0.0f};
+    float t{0.0f};
+
+    // The waypoint service server that sets the position and attitude waypoints at a given target
+    rclcpp::Service<pegasus_msgs::srv::SystemID>::SharedPtr systemID_service_{nullptr};
+
+    rclcpp::Publisher<geometry_msgs::msg::Vector3Stamped>::SharedPtr attitude_target_publisher_;
+    // Create Vector3Stamped message
+    geometry_msgs::msg::Vector3Stamped euler_msg;
+};
+
+}
---
+++ pegasus_autopilot/autopilot_modes/src/mode_systemID.cpp
@@ -0,0 +1,129 @@
+#include "autopilot_modes/mode_systemID.hpp"
+#include <pegasus_utils/rotations.hpp>
+
+
+namespace autopilot {
+
+SystemIdentification::~SystemIdentification() {
+    // Terminate the waypoint service
+    this->systemID_service_.reset();
+}
+
+void SystemIdentification::initialize() {
+
+    // Create the waypoint service server
+    node_->declare_parameter<std::string>("autopilot.SystemIdentification.set_settings_service", "set_settings"); 
+    this->systemID_service_ = this->node_->create_service<pegasus_msgs::srv::SystemID>(node_->get_parameter("autopilot.SystemIdentification.set_settings_service").as_string(), std::bind(&SystemIdentification::systemID_callback, this, std::placeholders::_1, std::placeholders::_2));
+    RCLCPP_INFO(this->node_->get_logger(), "SystemIdentification initialized");
+    attitude_target_publisher_ = this->node_->create_publisher<geometry_msgs::msg::Vector3Stamped>("attitude_target", 10);
+
+    // Get the mass of the vehicle (used to get the thrust from the acceleration)
+    VehicleConstants vehicle_constansts = get_vehicle_constants();
+    this->mass_ = vehicle_constansts.mass;
+}
+
+bool SystemIdentification::enter() {
+
+    // Check if the waypoint was already set - if not, then do not enter the waypoint mode
+    if (!this->settings_set_) {
+        RCLCPP_ERROR(this->node_->get_logger(), "Settings not set - cannot enter System Identification mode.");
+        return false;
+    }
+
+    // Reset the settings flag (to make sure we do not enter twice in this mode without setting new settings)
+    this->settings_set_ = false;
+
+    // Return true to indicate that the mode has been entered successfully
+    return true;
+}
+
+bool SystemIdentification::exit() {
+    
+    // Nothing to do here
+    return true;   // Return true to indicate that the mode has been exited successfully
+}
+
+void SystemIdentification::update(double dt) {
+    // Get the current state of the vehicle
+    State state = get_vehicle_state();
+    if (t < 5.0) {
+        // Get the vehicle to hover at the origin of the referential.
+        this->controller_->set_position({0.0, 0.0, -1.5}, 0.0, dt);
+
+        // Generate the Vector3Stamped message
+        euler_msg.header.stamp = rclcpp::Clock(RCL_SYSTEM_TIME).now();
+        euler_msg.header.frame_id = "base_link"; // Set appropriate frame_id
+
+        euler_msg.vector.x = 0; // roll
+        euler_msg.vector.y = 0; // pitch
+        euler_msg.vector.z = 0; // yaw
+
+        // Publish the Vector3Stamped message
+        attitude_target_publisher_->publish(euler_msg);
+    } else if (t < 6.0) {
+        Eigen::Vector3d attitude_target = compute_attitude(t);
+        double T = 9.83 * this->mass_ / std::abs(std::cos(attitude_target[0] * M_PI / 180 + attitude_target[1] * M_PI / 180));
+
+        this->controller_->set_attitude(attitude_target, T, dt);
+
+        // Set the header (timestamp and frame_id)
+        euler_msg.header.stamp = rclcpp::Clock(RCL_SYSTEM_TIME).now();
+        euler_msg.header.frame_id = "base_link"; // Set appropriate frame_id
+
+        // Set the vector values (roll, pitch, yaw)
+        euler_msg.vector.x = attitude_target[0]; // roll
+        euler_msg.vector.y = attitude_target[1]; // pitch
+        euler_msg.vector.z = attitude_target[2]; // yaw
+
+        // Publish the Vector3Stamped message
+        attitude_target_publisher_->publish(euler_msg);
+    }
+    else {
+        Eigen::Vector3d stop_position = state.position;
+        this->controller_->set_position({stop_position[0], stop_position[1], -1.5}, 0.0, dt);
+        // this->controller_->set_position({0.0, 0.0, -1.5}, 0.0, dt);
+    }
+
+    this->t += dt; // Increment time
+}
+
+// Funtion to compute the attitude target to be sent to the controabs(ller based on a sinusoidal
+Eigen::Vector3d SystemIdentification::compute_attitude(double t) {
+
+    // Initialize the unit vector
+    Eigen::Vector3d attitude = {0.0, 0.0, 0.0};       // Set the z component to 1.0 (to ensure hover mode, cos(~=0) = 1)
+
+    // Calculate the sinusoidal attitude for the specified axis
+    // double sinValue = std::sin(2*M_PI*this->frequency * t);
+
+    // Original: attitude[this->axis] = sinValue;
+    // attitude[0] = 0.173 * this->axis[0] * sinValue ;
+    attitude[0] = this->amplitude * this->axis[0];
+    attitude[1] = this->amplitude * this->axis[1];
+    return attitude;
+}
+
+void SystemIdentification::systemID_callback(const pegasus_msgs::srv::SystemID::Request::SharedPtr request, const pegasus_msgs::srv::SystemID::Response::SharedPtr response) {
+    
+    // Set the settings
+    this->axis[0] = request->axis[0];
+    this->axis[1] = request->axis[1];
+    this->axis[2] = request->axis[2];
+    this->amplitude = request->amplitude;
+    this->t = 0.0;          // Reset the time to 0.0
+
+
+    // Set the settings flag
+    this->settings_set_ = true;
+
+    // Return true to indicate that the settings has been set successfully
+    response->success = true;
+    RCLCPP_WARN(this->node_->get_logger(), "Axis oscilation direction set to (%f, %f, %f) with amplitude %f", this->axis[0], this->axis[1], this->axis[2], this->amplitude);
+}
+
+
+
+} // namespace autopilot
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(autopilot::SystemIdentification, autopilot::Mode)
